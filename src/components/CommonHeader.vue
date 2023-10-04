e0a2ebbe83ab348707ff1ab45cc1233b8e09f407
guandimumaomu <guandimumaomu@126.com>
2023-10-04 12:17:53 +0800

    面包屑实现

--- src/components/CommonHeader.vue
+++ src/components/CommonHeader.vue
@@ -2,7 +2,11 @@
   <header>
     <div class="l-content">
       <el-button @click="handleMenu" plain icon="el-icon-menu" size="mini"></el-button>
-      <h3 style="color: #fff">首页</h3>
+<!--      <h3 style="color: #fff">首页</h3>-->
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item  v-for="item in tags" :key="item.path"
+                             :to="{ path: item.path }">{{ item.label }}</el-breadcrumb-item>
+      </el-breadcrumb>
     </div>
     <div class="r-content">
       <el-dropdown trigger="click" size="mimi">
@@ -22,6 +26,8 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   name: 'CommonHeader',
   data () {
@@ -33,6 +39,13 @@ export default {
     handleMenu () {
       this.$store.commit('collapseMenu')
     }
+  },
+
+  computed: {
+    ...mapState({
+      // tab代表模块
+      tags: state => state.tab.tabList
+    })
   }
 }
 </script>
--- src/store/tab.js
+++ src/store/tab.js
@@ -1,10 +1,34 @@
 export default {
   state: {
-    isCollapse: false
+    isCollapse: false,
+    tabList: [
+      {
+        path: '/',
+        name: 'home',
+        label: '首页',
+        icon: 'home'
+      }
+    ],
+
+    currentMenu: null // 高亮显示
   },
   mutations: {
     collapseMenu (state) {
       state.isCollapse = !state.isCollapse
+    },
+    // 每次点击左侧菜单时，都需要改变tabList
+    selectMenu (state, val) {
+      if (val.name !== 'home') {
+        state.currentMenu = val
+        // 判断原有tabList里存不存在这个路由
+        const result = state.tabList.findIndex(item => item.name === val.name)
+        if (result === -1) {
+          state.tabList.push(val)
+        }
+      } else {
+        // 如果是首页，将当前选中的标识重置
+        state.currentMenu = null
+      }
     }
   }
 }
